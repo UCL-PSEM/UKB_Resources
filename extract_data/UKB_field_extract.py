09e3f22e14a4f1489fa19fe0e3ef467fba95137a
Carole Sudre <carolesudre@Caroles-MacBook-Pro-3.local>
2025-04-07 11:35:05 +0100

    Adding documentation

--- extract_data/UKB_field_extract.py
+++ extract_data/UKB_field_extract.py
@@ -4,6 +4,13 @@ import argparse
 import sys
 
 def create_list_fields(field, instance, idx_max):
+    """
+    From a given field, instance and maximum of extracted indices, create the list of complete field names
+    :param: field
+    :param: instance of the specified field
+    :param: idx_max: maximum index to extract   
+    :return: list of complete field names to be extracted    
+    """
     # field = x['field']
     # instance = x['instance']
     # idx_max = x['idx_max']
@@ -13,6 +20,11 @@ def create_list_fields(field, instance, idx_max):
     return list_fields
 
 def create_listcols(df_list):
+    """
+    From the dataframe containing the details of the fields to extract, create the list of fields to be extracted
+    :param: df_list
+    :return: list of complete field names
+    """
     df_list_unique = df_list[df_list['idx_max']==0]
     df_list_multi = df_list[df_list['idx_max']>0]
     df_list_unique['final_field'] = df_list_unique['field'].astype(str)+'-'+df_list_unique['instance'].astype(str)+'.0'
@@ -23,10 +35,16 @@ def create_listcols(df_list):
     return list_tot
 
 def select_columns(df, list_fields):
+    """
+    Given the DataFrame containing the data and the list of field create a selected view and return it
+    """
     selected_df = df[list_fields]
     return selected_df
 
 def encode_columns(df_sel, df_encode):
+    """
+    Using the selected dataframe and the dataframe of encoding, encode specific fields
+    """
     list_field = list(df_encode['field'])
     list_code = list(df_encode['code'])
     list_value = list(df_encode['value'])
@@ -47,11 +65,11 @@ def encode_columns(df_sel, df_encode):
 def main(argv):
     parser = argparse.ArgumentParser()
        
-    parser.add_argument('-file', dest='file', required=True)
-    parser.add_argument('-head',dest='header',required=True)
-    parser.add_argument('-fields',dest='fields',required=True)
-    parser.add_argument('-encoding',dest='encoding',default='')
-    parser.add_argument('-name',dest='name')
+    parser.add_argument('-file', dest='file', required=True, help='File (portion of original table) from which to extract data')
+    parser.add_argument('-head',dest='header',required=True, help='header file to use to indicate meaning of different columns')
+    parser.add_argument('-fields',dest='fields',required=True, help='csv file indicating the field, instance and indices to extract ')
+    parser.add_argument('-encoding',dest='encoding',default='',help='If needed path to csv file with encoding to be made of specific fields - ')
+    parser.add_argument('-name',dest='name',default='extracted',help='suffix to add to the original file to create the final extracted file')
     
     
     args = parser.parse_args(argv)
@@ -70,11 +88,11 @@ def main(argv):
 
 
 if __name__ == "__main__":
-    arg_array = ['-file','/Users/carolesudre/Data/UKB/splitnew_aa',
-                 '-head','/Users/carolesudre/Data/UKB/splitnew_aa',
-                 '-fields','/Users/carolesudre/Data/UKB/fields_Xin.csv',
-                 '-name','extractXin2',
-                 '-encoding','/Users/carolesudre/Data/UKB/encoding_field.csv']
-    main(arg_array) 
+    # arg_array = ['-file','/Users/carolesudre/Data/UKB/splitnew_aa',
+    #              '-head','/Users/carolesudre/Data/UKB/splitnew_aa',
+    #              '-fields','/Users/carolesudre/Data/UKB/fields_Xin.csv',
+    #              '-name','extractXin2',
+    #              '-encoding','/Users/carolesudre/Data/UKB/encoding_field.csv']
+    # main(arg_array) 
     #/Users/carolesudre/Development/UKB_reading.py
-    #main(sys.argv[1:])
+    main(sys.argv[1:])
